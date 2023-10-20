b7da5cc4153b986c66d9a8f44c5afc191e207c79
garenpham <tanphamminh2002@gmail.com>
2023-10-19 19:55:54 -0700

    Revert "Revert "Merge remote-tracking branch 'origin/v1' into tan""
    This reverts commit dbf27ff35c9f1a940dbb259f7671276ed1bcb785.

--- frontend/src/components/Media/MediaList.tsx
+++ frontend/src/components/Media/MediaList.tsx
@@ -2,36 +2,121 @@ import { Dispatch, SetStateAction } from 'react'
 import MediaItem from './MediaItem'
 
 interface Props {
-  setPreviewData: Dispatch<SetStateAction<{ src: string; id: string }>>
-  previewData: { src: string; id: string }
+  setPreviewData?: Dispatch<SetStateAction<{ src: string; id: string }>>
+  previewData?: { src: string; id: string }
   className?: string
+  updateCompareList?: (src: string, method: string) => void
+  compareList?: string[]
 }
 
 const style = {
   date: `col-span-2 text-center text-grey_2 font-[300]`,
 }
 
-function MediaList({ previewData, setPreviewData, className }: Props) {
+function MediaList({
+  compareList,
+  previewData,
+  setPreviewData,
+  className,
+  updateCompareList,
+}: Props) {
   return (
     <div
-      className={`${className} grid grid-cols-2 gap-x-[28px] gap-y-[18px] justify-start overflow-y-auto`}
+      className={`${className} grid grid-cols-2 gap-x-[28px] gap-y-[28px] justify-start overflow-y-auto`}
     >
       <div className={style.date}>03/01/2023</div>
-      <MediaItem id={'1'} setPreviewData={setPreviewData} video tag />
-      <MediaItem id={'2'} setPreviewData={setPreviewData} />
-      <MediaItem id={'3'} setPreviewData={setPreviewData} />
-      <MediaItem id={'4'} setPreviewData={setPreviewData} video tag />
+      <MediaItem
+        id={'1'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+        video
+        tag
+      />
+      <MediaItem
+        id={'2'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+      />
+      <MediaItem
+        id={'3'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+      />
+      <MediaItem
+        id={'4'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+        video
+        tag
+      />
       <div className={style.date}>02/01/2023</div>
-      <MediaItem id={'5'} setPreviewData={setPreviewData} />
-      <MediaItem id={'6'} setPreviewData={setPreviewData} tag />
-      <MediaItem id={'7'} setPreviewData={setPreviewData} video tag />
-      <MediaItem id={'8'} setPreviewData={setPreviewData} video tag />
-      <MediaItem id={'9'} setPreviewData={setPreviewData} />
-      <MediaItem id={'10'} setPreviewData={setPreviewData} tag />
+      <MediaItem
+        id={'5'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+      />
+      <MediaItem
+        id={'6'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+        tag
+      />
+      <MediaItem
+        id={'7'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+        video
+        tag
+      />
+      <MediaItem
+        id={'8'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+        video
+        tag
+      />
+      <MediaItem
+        id={'9'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+      />
+      <MediaItem
+        id={'10'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+        tag
+      />
       <div className={style.date}>01/01/2023</div>
-      <MediaItem id={'11'} setPreviewData={setPreviewData} />
-      <MediaItem id={'12'} setPreviewData={setPreviewData} />
-      <MediaItem id={'13'} setPreviewData={setPreviewData} video tag />
+      <MediaItem
+        id={'11'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+      />
+      <MediaItem
+        id={'12'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+      />
+      <MediaItem
+        id={'13'}
+        setPreviewData={setPreviewData}
+        updateCompareList={updateCompareList}
+        compareList={compareList}
+        video
+        tag
+      />
     </div>
   )
 }
