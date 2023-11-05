631c639b5b41325f0f0f8e93d90613d886bab4a4
garenpham <tanphamminh2002@gmail.com>
2023-11-04 21:20:29 -0700

    dark mode

--- frontend/src/components/DarkSwitch.tsx
+++ frontend/src/components/DarkSwitch.tsx
@@ -14,7 +14,6 @@ const style = {
   },
 }
 function DarkSwitch({}: Props) {
-  const [isActive, setIsActive] = useState(false)
   const [isMounted, setIsMounted] = useState(false)
   const { resolvedTheme, setTheme } = useTheme()
 
@@ -29,16 +28,17 @@ function DarkSwitch({}: Props) {
   return (
     <div
       className={`relative w-[60px] h-[28px] rounded-l-[4px] rounded-r-[4px] flex items-center px-[.2rem] ${
-        isActive ? style.container.active : style.container.inactive
+        resolvedTheme === 'light'
+          ? style.container.active
+          : style.container.inactive
       } active:translate-y-[.2rem] transition-all ease-linear group`}
       onClick={() => {
-        setIsActive((prev) => !prev)
         setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
       }}
     >
       <div
         className={`relative z-10 bg-white h-[22px] w-[22px] rounded-md ${
-          !isActive && 'group-hover:bg-yellow-400'
+          resolvedTheme === 'light' && 'group-hover:bg-yellow-400'
         }`}
       />
 
