0f5ce5ea5071cf398171b114ad49601d54330310
garenpham <tanphamminh2002@gmail.com>
2023-11-06 10:48:59 -0800

    icons for ImageCanvas

--- frontend/src/components/ImageCanvas.tsx
+++ frontend/src/components/ImageCanvas.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useGlobalContext } from '@/context/global-context'
+import { AiOutlineExpandAlt } from 'react-icons/ai'
 import { GoShare } from 'react-icons/go'
 import { HiMagnifyingGlassMinus, HiMagnifyingGlassPlus } from 'react-icons/hi2'
 import { SlPencil } from 'react-icons/sl'
@@ -16,17 +17,20 @@ function ImageCanvas({}: Props) {
   const { previewMedia } = useGlobalContext()
   const { src, IsRightEye } = previewMedia
   return (
-    <div className="grow flex">
-      <div className={`ml-[1rem] mt-[1rem] w-[1040px] h-fit`}>
+    <div className="grow flex h-fit">
+      <div className={`ml-[1rem] mt-[1rem] w-[1040px]`}>
         <Img src={src} className="!object-contain" />
       </div>
 
-      <div className="grow flex flex-col items-center mt-[2rem] gap-y-[1.4rem]">
-        <HiMagnifyingGlassPlus className={`${style.icon} text-[40px]`} />
-        <HiMagnifyingGlassMinus className={`${style.icon} text-[40px]`} />
-        <SlPencil className={`${style.icon} text-[34px]`} />
-        <VscSettings className={`${style.icon} text-[34px]`} />
-        <GoShare className={`${style.icon} text-[34px]`} />
+      <div className="grow flex flex-col items-center justify-between mt-[2rem] mb-[1rem]">
+        <div className={`flex flex-col items-center gap-y-[1.4rem]`}>
+          <HiMagnifyingGlassPlus className={`${style.icon} text-[40px]`} />
+          <HiMagnifyingGlassMinus className={`${style.icon} text-[40px]`} />
+          <SlPencil className={`${style.icon} text-[34px]`} />
+          <VscSettings className={`${style.icon} text-[34px]`} />
+          <GoShare className={`${style.icon} text-[34px]`} />
+        </div>
+        <AiOutlineExpandAlt className={`${style.icon} text-[40px]`} />
       </div>
     </div>
   )
