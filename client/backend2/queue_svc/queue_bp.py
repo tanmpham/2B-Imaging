97650215051513c730e71c5bf34c0ffd154b03f2
garenpham <tanphamminh2002@gmail.com>
2023-11-13 13:48:58 -0800

    kafka demo

--- client/backend2/queue_svc/queue_bp.py
+++ client/backend2/queue_svc/queue_bp.py
@@ -13,6 +13,7 @@ import logging.config
 from threading import Thread
 from queue_svc.db_actions.imagetags import add_tag
 import uuid
+import queue
 
 queue_bp = Blueprint("queue", __name__)
 
@@ -57,22 +58,31 @@ hostname = "%s:%d" % (
 )
 
 
+# Create a Queue object
+q_producer = queue.Queue()
+q_consumer = queue.Queue()
+
+
 def connect_kafka():
-    for connecting in range(appConfig["max_tries"]):
-        try:
-            client = KafkaClient(hosts=hostname)
-            topic = client.topics[str.encode(appConfig["events"]["topic"])]
-            return topic
-        except Exception:
-            print(f"[producer]: Numbers of fail connection to Kafka: {connecting}")
-            time.sleep(appConfig["reconnect_sleep"])
-            continue
+    # for connecting in range(appConfig["max_tries"]):
+    try:
+        client = KafkaClient(hosts=hostname)
+        topic = client.topics[str.encode(appConfig["events"]["topic"])]
+        # Put the topic in the queue
+        q_producer.put(topic)
+        q_consumer.put(topic)
+        return
+    except Exception:
+        print(f"[connect_kafka]: Fail connection")
+        # time.sleep(appConfig["reconnect_sleep"])
+        # continue
 
 
 def produce_msg():
     try:
-        topic = connect_kafka()
+        topic = q_producer.get_nowait()
         producer = topic.get_sync_producer()
+        print(f"[produce_msg]: Connected to Kafka.")
         # Check if file exists
         if os.path.isfile(queue_json):
             with open(queue_json, "r") as file:
@@ -96,8 +106,8 @@ def produce_msg():
                     logger.info(f"[producer]: Error while producing message.")
             else:
                 print("No new messages")
-    except Exception:
-        logger.info(f"[producer]: Cannot sync producer.")
+    except queue.Empty:
+        logger.info(f"[producer]: No connection to Kafka.")
 
 
 def save_msg(message):
@@ -158,37 +168,46 @@ def read_received_msg():
 
 
 def consume_msg():
-    try:
-        topic = connect_kafka()
-        # Create a consume on a consumer group, that only reads new messages
-        # (uncommitted messages) when the service re-starts (i.e., it doesn't
-        # read all the old messages from the history in the message queue).
+    while True:
+        try:
+            topic = q_consumer.get_nowait()
+            # Create a consume on a consumer group, that only reads new messages
+            # (uncommitted messages) when the service re-starts (i.e., it doesn't
+            # read all the old messages from the history in the message queue).
 
-        consumer = topic.get_simple_consumer(
-            consumer_group=json.dumps(producerID).encode("utf-8"),
-            reset_offset_on_start=False,
-            auto_offset_reset=OffsetType.LATEST,
-        )
+            consumer = topic.get_simple_consumer(
+                consumer_group=json.dumps(producerID).encode("utf-8"),
+                reset_offset_on_start=False,
+                auto_offset_reset=OffsetType.LATEST,
+            )
 
-        for msg in consumer:
-            msg_str = msg.value.decode("utf-8")
-            msg = json.loads(msg_str)
-            logger.info("Consumed Message: %s" % msg)
+            print(f"[consume_msg]: Connected to Kafka.")
 
-            if msg["producerID"] == producerID:
-                logger.info("[consumer]: Same producer id in this client")
-            else:
-                save_msg(msg)
+            for msg in consumer:
+                msg_str = msg.value.decode("utf-8")
+                msg = json.loads(msg_str)
+                logger.info("Consumed Message: %s" % msg)
 
-            # Commit the new message as being read
-            consumer.commit_offsets()
+                if msg["producerID"] == producerID:
+                    logger.info("[consume_msg]: Same producer id in this client")
+                else:
+                    save_msg(msg)
 
-    except Exception:
-        logger.info(f"[consumer]: Cannot connect to the consumer group.")
+                # Commit the new message as being read
+                consumer.commit_offsets()
+
+        except queue.Empty:
+            logger.info(f"[consume_msg]: No connection to Kafka.")
+            time.sleep(appConfig["reconnect_sleep"])
 
 
 def init_scheduler():
     sched = BackgroundScheduler(daemon=True)
+    sched.add_job(
+        connect_kafka,
+        "interval",
+        seconds=appConfig["reconnect_sleep"],
+    )
     sched.add_job(
         produce_msg,
         "interval",
@@ -206,6 +225,10 @@ def init_scheduler():
 
 init_scheduler()
 
-t1 = Thread(target=consume_msg)
-t1.setDaemon(True)
-t1.start()
+# t1 = Thread(target=connect_kafka)
+# t1.setDaemon(True)
+# t1.start()
+
+t2 = Thread(target=consume_msg)
+t2.setDaemon(True)
+t2.start()
