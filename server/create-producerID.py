a7e88c290c789d47f0b3a54bf8f6bff0697e82f0
garenpham <tanphamminh2002@gmail.com>
2023-12-12 05:28:15 -0800

    cleanup

--- server/create-producerID.py
+++ server/create-producerID.py
@@ -7,20 +7,6 @@ c = conn.cursor()
 
 producerID = str(uuid.uuid4())
 
-c.execute(
-    """
-        DROP TABLE IF EXISTS `producer`;
-    """
-)
-
-c.execute(
-    """
-          CREATE TABLE `producer` (
-              `ProducerID` VARCHAR(36) PRIMARY KEY
-          );
-    """
-)
-
 query = """
         INSERT INTO producer (ProducerID) VALUES (%s); 
     """
