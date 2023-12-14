92b05aa72e52bdfb1bb2b239254e0868da8c2097
garenpham <tanphamminh2002@gmail.com>
2023-12-14 08:42:28 -0800

    add media title

--- frontend/src/app/add-media/_components/add-media.tsx
+++ frontend/src/app/add-media/_components/add-media.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import React from 'react'
+import { MdDriveFolderUpload } from 'react-icons/md'
+import UploadImageSection from './upload-image'
 
 const style = {
   type: {
-    active: `text-[24px] w-[140px] flex justify-center py-[1rem] border border-white`,
+    active: `text-[24px] w-[140px] flex justify-center py-[1rem] border border-white active:scale-95 hover:opacity-90 transition-all ease-linear`,
     inactive: `text-[24px] w-[140px] flex justify-center py-[1rem] text-stone-600 hover:text-stone-400 border border-transparent hover:border-stone-400 cursor-pointer active:scale-95 transition-all ease-linear`,
   },
 }
@@ -15,9 +17,11 @@ function AddMediaPage({}: Props) {
   const [isImage, setIsImage] = React.useState(true)
   return (
     <div className="text-black dark:text-white m-auto bg-grey_2 dark:bg-grey_3 rounded-[10px] w-[84vw] h-[94vh]">
-      <h1 className="text-[34px] my-[1rem] mx-[2rem]">Upload</h1>
+      <h1 className="text-[34px] my-[1rem] mx-[2rem] flex gap-x-[1rem] items-center">
+        Upload <MdDriveFolderUpload className="text-[40px]" />
+      </h1>
       <div className="h-[1px] w-full bg-black dark:bg-white" />
-      <div className={`flex`}>
+      <div className={`flex mb-[1rem]`}>
         <div
           className={isImage ? style.type.active : style.type.inactive}
           onClick={() => setIsImage(true)}
@@ -31,6 +35,8 @@ function AddMediaPage({}: Props) {
           Video
         </div>
       </div>
+
+      <UploadImageSection />
     </div>
   )
 }
---
+++ frontend/src/app/add-media/_components/upload-image.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+function UploadImageSection({}: Props) {
+  return <div>Image</div>
+}
+
+export default UploadImageSection
